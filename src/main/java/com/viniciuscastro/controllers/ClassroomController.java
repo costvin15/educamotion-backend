72a2efcbf9aa2b91fe8520a026be0241f3017be4
Vinicius Costa Castro <costacastrovinicius7@gmail.com>
2025-01-01 23:52:25 -0300

    feat: Fetching user information with Keycloak

--- src/main/java/com/viniciuscastro/controllers/ClassroomController.java
+++ src/main/java/com/viniciuscastro/controllers/ClassroomController.java
@@ -1,91 +1,76 @@
 package com.viniciuscastro.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
 
 import com.viniciuscastro.models.classroom.Classroom;
+import com.viniciuscastro.services.ClassroomAttendanceService;
 import com.viniciuscastro.services.ClassroomService;
 import com.viniciuscastro.services.UserService;
 
-import io.quarkus.logging.Log;
 import io.quarkus.security.Authenticated;
 import io.quarkus.websockets.next.OnClose;
 import io.quarkus.websockets.next.OnError;
 import io.quarkus.websockets.next.OnOpen;
 import io.quarkus.websockets.next.WebSocket;
 import io.quarkus.websockets.next.WebSocketConnection;
-import io.quarkus.websockets.next.UserData.TypedKey;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+
 @Authenticated
 @Path("classroom")
 @WebSocket(path = "/classroom-controller/{classroomId}")
 public class ClassroomController {
     @Inject
-    ClassroomService service;
+    ClassroomService classroomService;
+
+    @Inject
+    ClassroomAttendanceService classroomAttendanceService;
 
     @Inject
     UserService userService;
 
-    Map<String, List<WebSocketConnection>> connections = new ConcurrentHashMap<>();
-
     @GET
-    @Path("get/{presentationId}")
-    public Classroom getClassroom(String presentationId) {
-        Optional<Classroom> classroom = this.service.getClassroomByPresentationId(presentationId);
+    @Path("get/{classroomId}")
+    public Classroom getClassroom(String classroomId) {
+        Optional<Classroom> classroom = this.classroomService.getClassroomByClassroomId(classroomId);
         if (classroom.isPresent()) {
             return classroom.get();
         }
         return null;
     }
 
+    @GET
+    @Path("attendances/{classroomId}")
+    public List<String> getAttendances(String classroomId) {
+        return this.classroomService.getAttendances(classroomId);
+    }
+
     @POST
     @Path("create/{presentationId}")
     public Classroom createClassroom(String presentationId) {
-        return this.service.createClassroom(presentationId);
+        return this.classroomService.createClassroom(presentationId);
     }
 
     @OnOpen()
     public void onOpen(WebSocketConnection connection) {
         String classroomId = connection.pathParam("classroomId");
-        Log.info("Connection opened with classroomId: " + classroomId);
-        connection.userData().put(TypedKey.forString("userId"), userService.getUserId());
-
-        if (!service.getClassroomByClassroomId(classroomId).isPresent()) {
-            connection.closeAndAwait();
-            return;
-        }
-
-        if (!connections.containsKey(classroomId)) {
-            connections.put(classroomId, new ArrayList<>());
-        }
-
-        connections.get(classroomId).add(connection);
+        String userId = this.userService.getUserId();
+        this.classroomAttendanceService.addAttendance(classroomId, userId, connection);
     }
 
     @OnClose
     public void onClose(WebSocketConnection connection) {
         String classroomId = connection.pathParam("classroomId");
-        this.removeConnection(classroomId, connection);
+        this.classroomAttendanceService.removeAttendance(classroomId, connection);
         connection.close();
     }
 
     @OnError
     public void onError(Throwable throwable, WebSocketConnection connection) {
         String classroomId = connection.pathParam("classroomId");
-        this.removeConnection(classroomId, connection);
-    }
-
-    private void removeConnection(String classroomId, WebSocketConnection connection) {
-        if (!connections.containsKey(classroomId)) {
-            return;
-        }
-
-        connections.get(classroomId).remove(connection);
+        this.classroomAttendanceService.removeAttendance(classroomId, connection);
     }
 }
--- src/main/java/com/viniciuscastro/services/ClassroomService.java
+++ src/main/java/com/viniciuscastro/services/ClassroomService.java
@@ -1,14 +1,22 @@
 package com.viniciuscastro.services;
 
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.representations.idm.UserRepresentation;
 
 import com.viniciuscastro.models.classroom.Classroom;
 import com.viniciuscastro.models.presentations.Presentation;
 import com.viniciuscastro.repositories.ClassroomRepository;
 
+import io.quarkus.logging.Log;
+import io.quarkus.websockets.next.WebSocketConnection;
+import io.quarkus.websockets.next.UserData.TypedKey;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
@@ -23,6 +31,12 @@ public class ClassroomService {
 
     @Inject
     PresentationService presentationService;
+
+    @Inject
+    ClassroomAttendanceService classroomAttendanceService;
+
+    @Inject
+    Keycloak keycloak;
 
     public Optional<Classroom> getClassroomByPresentationId(String presentationId) {
         return repository.findByUserIdAndPresentationId(userService.getUserId(), presentationId);
@@ -60,5 +74,23 @@ public class ClassroomService {
             throw new RuntimeException("Error creating classroom");
         }
         return createdClassroom.get();
+    }
+
+    public List<String> getAttendances(String classroomId) {
+        Optional<Classroom> classroom = this.getClassroomByClassroomId(classroomId);
+        if (!classroom.isPresent()) {
+            throw new RuntimeException("Classroom does not exist");
+        }
+
+        Log.info(this.classroomAttendanceService.getClassroomAttendances(classroomId).size());
+
+        List<String> attendances = new ArrayList<>();
+        for (WebSocketConnection connection : this.classroomAttendanceService.getClassroomAttendances(classroomId)) {
+            String userId = connection.userData().get(TypedKey.forString("userId"));
+            UserRepresentation user = keycloak.realm("educamotion").users().get(userId).toRepresentation();
+            attendances.add(user.getUsername());
+        }
+
+        return attendances;
     }
 }
