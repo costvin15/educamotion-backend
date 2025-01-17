700a68bb26263e2a928b42fab3a60834974bddae
Vinicius Costa Castro <costacastrovinicius7@gmail.com>
2025-01-17 17:41:11 -0300

    feat: Update interactive object suggestions in OpenAIService interface

--- src/main/java/com/viniciuscastro/interfaces/OpenAIService.java
+++ src/main/java/com/viniciuscastro/interfaces/OpenAIService.java
@@ -15,10 +15,11 @@ import io.quarkiverse.langchain4j.guardrails.OutputGuardrails;
 public interface OpenAIService {
     @SystemMessage("""
             Você é um assistente de IA que sugere objetos interativos que podem ser adicionados com base em uma imagem.
-            Os objetos interativos disponiveis são Questões objetivas, Questões subjetivas, Questões de Múltipla Escolha e Nuvem de Palavras.
+            Os tipos de objetos interativos disponiveis são Questões objetivas, Questões subjetivas, Questões de Múltipla Escolha e Nuvem de Palavras.
             """)
     @UserMessage("""
-            Descreva a imagem e sugira objetos interativos. A resposta deve conter apenas JSON com dados de objetos interativos, com nenhuma outra frase.
+            Descreva a imagem e sugira objetos interativos. Não é necessário gerar todos os tipos de objeto interativo.disponíveis, apenas os que fazem sentido para a imagem. É possível sugerir mais de um objeto interativo do mesmo tipo.
+            A resposta deve conter apenas JSON com dados de objetos interativos, com nenhuma outra frase.
             A resposta deve ser uma lista "interactive_objects" com objetos. Os campos obrigatórios de cada objeto são: "type" (enum: "QUESTION_OBJECTIVE", "QUESTION_SUBJECTIVE", "QUESTION_MULTIPLE_CHOICE", "WORD_CLOUD"), "question" (apenas para "QUESTION_OBJECTIVE", "QUESTION_SUBJECTIVE" e "QUESTION_MULTIPLE_CHOICE"),
             "options" (apenas para "QUESTION_OBJECTIVE" e "QUESTION_MULTIPLE_CHOICE"), "words" (apenas para "WORD_CLOUD"). O campo "words" deve ser um objeto contendo "value" e "weight".
             """)
